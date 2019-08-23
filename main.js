692083a942104794447230d32c757d9148f38d51
kiliy <kiliy@DESKTOP-TOP7ID7>
2019-08-23 15:58:46 +0530

    MySQL Pool

--- main.js
+++ main.js
@@ -1,33 +1,39 @@
 const mqtt = require('mqtt');
 const client = mqtt.connect('mqtt://192.168.8.10');
 const mysql = require('mysql');
-const connection = mysql.createConnection({
-	  host     : '192.168.8.10',
-	  user     : 'pi',
-	  password : 'raspberry',
-	  database : 'iot'
-	});
 
-
+var pool  = mysql.createPool({
+	  connectionLimit : 10,
+	  host            : '192.168.8.10',
+	  user            : 'pi',
+	  password        : 'raspberry',
+	  database        : 'iot',
+	  connectTimeout  : 60 * 60 * 1000,
+	  acquireTimeout  : 60 * 60 * 1000,
+	  timeout         : 60 * 60 * 1000
+});
 
 client.on('connect', () => {
 	console.log('Connected to MQTT Broker');
+	
 	client.subscribe('waterLevelTopic');
 	console.log('Subscribed to waterLevelTopic');
-	});
+
+});
 
 client.on('message', (topic, message) => {
 	  if(topic === 'waterLevelTopic') {
 		 console.log('Message: ' + message);
 		 
-		 connection.connect();
-		 let query = mysql.format('INSERT INTO ?? (??) VALUES (?)',["water_level_log", "value", message]);
-		 connection.query(query,function(err, rows, fields) {
-			  	if (!err)
-				    console.log('Logged to database');
-				  else
-				    console.log('Error while stroing to database');
-				});
-		 connection.end();
-	  }
-	})
+		 pool.getConnection(function(err, connection) {
+			 if (err) throw err; // not connected!
+			 
+			 let query = mysql.format('INSERT INTO ?? (??) VALUES (?)',["water_level_log", "value", message]);
+			 connection.query(query, function (error, results, fields) {
+				 connection.release();
+				 if (error) throw error;
+			 });
+			 
+		 });
+	}
+});
