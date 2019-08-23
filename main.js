d8b23845616355d3ed4b06c88b843ebb5b787bd9
kiliy <kiliy@DESKTOP-TOP7ID7>
2019-08-23 15:04:02 +0530

    Subscribes to waterLevelTopic and stores to database

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log('Initial commit');
+const mqtt = require('mqtt');
+const client = mqtt.connect('mqtt://192.168.8.10');
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+	  host     : '192.168.8.10',
+	  user     : 'pi',
+	  password : 'raspberry',
+	  database : 'iot'
+	});
+
+
+
+client.on('connect', () => {
+	console.log('Connected to MQTT Broker');
+	client.subscribe('waterLevelTopic');
+	console.log('Subscribed to waterLevelTopic');
+	});
+
+client.on('message', (topic, message) => {
+	  if(topic === 'waterLevelTopic') {
+		 console.log('Message: ' + message);
+		 
+		 connection.connect();
+		 let query = mysql.format('INSERT INTO ?? (??) VALUES (?)',["water_level_log", "value", message]);
+		 connection.query(query,function(err, rows, fields) {
+			  	if (!err)
+				    console.log('Logged to database');
+				  else
+				    console.log('Error while stroing to database');
+				});
+		 connection.end();
+	  }
+	})
